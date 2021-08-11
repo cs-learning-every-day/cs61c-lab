daed0b2efa3141b5ac6723dc90e88fd710482fe4
XmchxUp <1394466835@qq.com>
2021-08-11 20:05:16 +0800

    finish lab02 exercise3

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -2,18 +2,28 @@
 #include "bit_ops.h"
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
-unsigned get_bit(unsigned x, unsigned n) {
+unsigned get_bit(unsigned x, unsigned n)
+{
     /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    return (x >> n) & 0x1;
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
-void set_bit(unsigned *x, unsigned n, unsigned v) {
+void set_bit(unsigned *x, unsigned n, unsigned v)
+{
     /* YOUR CODE HERE */
+    unsigned nth_value = get_bit(*x, n);
+
+    // set nth value to zero
+    *x = *x & ~(nth_value << n);
+
+    // set nth value to v
+    *x = *x | (v << n);
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
-void flip_bit(unsigned *x, unsigned n) {
+void flip_bit(unsigned *x, unsigned n)
+{
     /* YOUR CODE HERE */
+    *x = *x ^ (1 << n);
 }
-
